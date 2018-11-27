d2588b62b4e5013b1e63de8a65426edfec2df000
Dustin Spicuzza <dustin@virtualroadside.com>
2018-11-27 10:16:40 -0500

    Format code using black

--- netconsole/__init__.py
+++ netconsole/__init__.py
@@ -1,11 +1,6 @@
-
 try:
     from .version import __version__
 except ImportError:
-    __version__ = 'master'
-    
-from .netconsole import (
-    Netconsole,
-    run
-)
-    
+    __version__ = "master"
+
+from .netconsole import Netconsole, run
--- netconsole/__main__.py
+++ netconsole/__main__.py
@@ -1,5 +1,4 @@
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     from .netconsole import main
-    main()
 
+    main()
